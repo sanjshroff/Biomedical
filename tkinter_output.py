819255ef90702d4c5ea6aaecc295320e91a70638
Vaishnavi <gonelavaishnavi21@gmail.com>
2022-12-13 12:41:59 -0400

    updated code with some changes

--- tkinter_output.py
+++ tkinter_output.py
@@ -7,24 +7,24 @@ Created on Wed Oct 26 16:53:45 2022
 import os
 import pandas as pd
 
-def display_title_scores(input_limit=4):
-    root = tk.Tk()
-    root.geometry("900x300")
+# def display_title_scores(input_limit=4):
+#     root = tk.Tk()
+#     root.geometry("900x300")
 
-    root.title("Most Relevant Documents")
+#     root.title("Most Relevant Documents")
 
-    with open(os.getcwd()+r"\output\sbert_topk.csv", "r", newline="") as row:
-        eachrow = csv.reader(row)
-        row_list = list(eachrow)
+#     with open(os.getcwd()+r"\output\sbert_topk.csv", "r", newline="") as row:
+#         eachrow = csv.reader(row)
+#         row_list = list(eachrow)
 
-    #taking minimum of user input and the cluster size to display those many relevent articles 
-    input_limit = min(input_limit,len(row_list))
+#     #taking minimum of user input and the cluster size to display those many relevent articles 
+#     input_limit = min(input_limit,len(row_list))
 
-    for i, row in enumerate(row_list):
-        if input_limit >= 0:
-            for col in [2, 3]:
-                tk.Label(root, text=row[col]).grid(row=i, column=col)
-        input_limit = input_limit - 1
+#     for i, row in enumerate(row_list):
+#         if input_limit >= 0:
+#             for col in [2, 3]:
+#                 tk.Label(root, text=row[col]).grid(row=i, column=col)
+#         input_limit = input_limit - 1
 #     root.mainloop()
 def display_title_scores(input_limit=7):
     df = pd.read_csv(os.getcwd()+r"\output\sbert_topk.csv")
